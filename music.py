718ef991a5ae9724e0bf977a2197d1c234221564
Fatima Nadeem <31242975+fat1nad@users.noreply.github.com>
2020-12-22 17:00:28 +0500

    Update music.py

--- music.py
+++ music.py
@@ -1,4 +1,4 @@
-from myimage import *
+from myimage import MyImage
 
 '''
 2D Array containing the music melodic sequence:
@@ -11,7 +11,8 @@ M[m][1] contains the duration that the note is played for;
 NN refers to null, or beats with silence. 
 Like a note, a silent beat will have a duration value.
 For example:
-M = [
+M = 
+[
   ('C3', 1/2), ('C3', 1/2), ('D3', 1), ('NN', 1), 
   ('C3', 1), ('NN', 1), ('E3', 1), ('C3', 2)
 ]
@@ -35,28 +36,54 @@ is used rather than flat representation (b)
 '''
 
 # The chromatic scale to color wheel mapping
-mapping = {
+mapping = 
+{
   'A': (255, 0, 0), 'A#': (255, 127, 0), 
   'B': (255, 255, 0), 'C': (127, 255, 0), 
   'C#': (0, 255, 0), 'D': (0, 255, 127),
   'D#': (0, 255, 255), 'E': (0, 127, 255), 
   'F': (0, 0, 255), 'F#': (127, 0, 255), 
   'G': (255, 0, 255), 'G#': (255, 0, 127)
+  'NN': (255, 255, 255)
 }
 
-# melodyArr is the array of melodic beats described above.
-# sig is the time signature of the music; 
-# e.g. 8 beats per measure, 16 beats per measure, etc.
-# key is the key the song is in; e.g. A, C, Dm, F#.
-def pixelColour(melodyArr: [], sig: int, key: str):
-    # Function to convert a melody to an image
+def PixelColour(melodyArr: [], sig: int, startingNote: str):
+    '''
+    Function to convert a melody to an image
+
+    Arguments:
+    melodyArr is the array of melodic beats described above.
+    sig is the time signature of the music; 
+    e.g. 8 beats per measure, 16 beats per measure, etc.
+    startingNote is the first note of the melody.
+    --key is the key the song is in; e.g. A, C, Dm, F#.--(REMOVED)
+
+    Returns: 
+    MyImage object
+
+    '''
+
+    x = len(melodyArr)
+    y = x // sig
+    img = MyImage((x, y), 0, 100, 'RGBA')
+    startingColor = mapping[startingNote]
+
+    for note in melodyArr:
+        
+
+
+    img.show()  
+    return img
 
     '''
     MUST DELETE FOLLOWING COMMENTS - ONLY FOR DEVELOPERS
-    1. Create an empty instance of myimage class with a large enough size for virtual pixels. x dimension of the image will be sig; y dimension will be size(melodyArr) // no of beats per measure
-    2. Determine the starting colour based on the root note of the music key. For example, if music is in the key of A, the starting colour is Red (use chromatic scale to colour wheel mapping).
-    3. Iterate over each ith element of the melodyArr, based on the sig value. For example, if the music is in a time signature of 8 beats, after every 7th iteration we start with 
-    4. 
+    1. Create an empty instance of myimage class with a large enough size for virtual pixels. 
+    x dimension of the image will be sig; y dimension will be size(melodyArr) // no of beats per measure
+    2. Determine the starting colour based on the root note of the music key. For example, 
+    if music is in the key of A, the starting colour is Red (use chromatic scale to colour wheel mapping).
+    3. Iterate over each ith element of the melodyArr, based on the sig value. 
+    For example, if the music is in a time signature of 8 beats, after every 7th iteration we start with 
+ 
     '''
     
 
